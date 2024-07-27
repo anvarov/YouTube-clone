7d7353b5d513a2f885722c8911191dcf654ab482
Akmal Anvarov <akmal@anvarov.dev>
2024-07-28 01:32:58 +0500

    add ffmpeg

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,30 @@
 import express  from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
 
-const PORT = 3000;
+app.use(express.json());
 
-app.get('/', (req, res) => {    
-    res.send('Hello World');
+app.post('/process-video', (req, res) => {
+    const inputeFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputeFilePath || !outputFilePath) {
+        res.status(400).send('inputFilePath and outputFilePath are required');
+    }
+    ffmpeg(inputeFilePath)
+        .outputOptions('-vf', 'scale=-1:360')
+        .on('end', () => {
+            res.status(200).send('Video processing complete');
+        })
+        .on('error', (err) => {
+            console.log('error occurred', err);
+            res.status(500).send(`Video processing failed: ${err}`);
+        })
+        .save(outputFilePath);
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
